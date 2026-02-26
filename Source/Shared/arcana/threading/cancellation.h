7bade81652426873a40e95e11cdfb6f6565d7d2b
Gary Hsu <bghgary@users.noreply.github.com>
2026-02-26 10:33:21 -0800

    Use thread local storage for cancellation none

--- Source/Shared/arcana/threading/cancellation.h
+++ Source/Shared/arcana/threading/cancellation.h
@@ -9,7 +9,6 @@
 #include <vector>
 
 #include <functional>
-#include <vector>
 
 namespace arcana
 {
@@ -129,34 +128,9 @@ namespace arcana
         std::atomic<bool> m_cancellationRequested{ false };
     };
 
-    namespace internal::no_destroy_cancellation
-    {
-        // To address a problem with static globals recognized as the same as in a 
-        // standards proposal, we adopt a workaround described in the proposal.
-        // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p1247r0.html
-        template <class T>
-        class no_destroy
-        {
-            alignas(T) unsigned char data_[sizeof(T)];
-
-        public:
-            template <class... Ts>
-            no_destroy(Ts&&... ts)
-            {
-                new (data_)T(std::forward<Ts>(ts)...);
-            }
-
-            T &get()
-            {
-                return *reinterpret_cast<T *>(data_);
-            }
-        };
-
-        inline no_destroy<cancellation_source> none{};
-    }
-
     inline cancellation& cancellation::none()
     {
-        return internal::no_destroy_cancellation::none.get();
+        static thread_local cancellation_source n{};
+        return n;
     }
 }
